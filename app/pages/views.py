e30068ebd272e82ab14a770bae1597c377601d93
Shivam Thapar <shivam.thapar1@gmail.com>
2014-11-17 10:34:41 -0800

    styling, new page, page switching

--- app/pages/views.py
+++ app/pages/views.py
@@ -19,11 +19,12 @@ from oauth2client.client import FlowExchangeError
 
 from simplekv.memory import DictStore
 from flask_kvsession import KVSessionExtension
+from sqlalchemy import and_
 
 from app import db
 from app.users.models import User
 from app.pages.models import Page
-import app.pages.constants
+from app.pages import constants as PAGE
 
 app = Flask(__name__)
 
@@ -58,6 +59,29 @@ def index():
   page = pages[0]
   return render_template('pages/main.html', pages=pages, page=page)
 
+@mod_pages.route('/<id>')
+def show(id):
+  access_token = session.get('credentials')
+  gplusId = session.get('gplus_id')
+
+  if access_token is None:
+    response = make_response(json.dumps('Current user not connected.'), 401)
+    response.headers['Content-Type'] = 'application/json'
+    return redirect(url_for('users.login'))
+  credentials = AccessTokenCredentials(access_token, 'user-agent-value')
+
+  # Only fetch a list of people for connected users.
+  if credentials is None:
+    response = make_response(json.dumps('Current user not connected.'), 401)
+    response.headers['Content-Type'] = 'application/json'
+    return redirect(url_for('users.login'))
+
+  pages = getPages()
+  # todo check for gplusId too
+  page = Page.query.filter_by(id=id).first()
+  return render_template('pages/main.html', pages=pages, page=page, id=id)
+
+
 def getPages():
   gplusId = session.get('gplus_id')
   query = Page.query.filter_by(gplusId=gplusId)
@@ -75,13 +99,18 @@ def createPage(title, content):
   db.session.commit()
   return page
 
-@mod_pages.route('/create', methods=['POST'])
+@mod_pages.route('/create', methods=['POST', 'GET'])
 def create():
-  title = request.form['title']
-  content = request.form['content']
-  if title is not None:
-    page = createPage(title,content)
-    print page
+  if(request.method == 'GET'):
+    title = PAGE.DEFAULT_PAGE_TITLE
+    content = ""
+  else:
+    title = request.form['title']
+    content = request.form['content']
+  if title is None:
+    title = PAGE.DEFAULT_PAGE_TITLE
+  page = createPage(title,content)
+  print page
   return redirect(url_for('pages.index'))
 
 def updatePage(pageId, title, content):
